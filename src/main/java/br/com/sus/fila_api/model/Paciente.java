decd721f8ebb29d9bb115b5aa24c58b9619dffb9
Victor Mateus <8victor49@gmail.com>
2026-05-03 12:26:22 -0300

    Criação das Entitys

--- src/main/java/br/com/sus/fila_api/model/Paciente.java
+++ src/main/java/br/com/sus/fila_api/model/Paciente.java
@@ -1,11 +1,22 @@
 package br.com.sus.fila_api.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDate;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Paciente {
 
     @Id
+    private String cpf;
+
+    private String nome;
+    private LocalDate dataNacimento;
+    private String telefone;
+    private String email;
+    private String endereço;
 
 }
